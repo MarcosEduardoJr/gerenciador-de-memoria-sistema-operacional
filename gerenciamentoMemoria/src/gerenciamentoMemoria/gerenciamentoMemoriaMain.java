d35788959625765b59d18612b3ceb2656db71d16
BarbaLoira <marcoseduflautista1@gmail.com>
2017-07-14 22:19:56 -0300

    visualizacao resultado

--- gerenciamentoMemoria/src/gerenciamentoMemoria/gerenciamentoMemoriaMain.java
+++ gerenciamentoMemoria/src/gerenciamentoMemoria/gerenciamentoMemoriaMain.java
@@ -17,38 +17,71 @@ public class gerenciamentoMemoriaMain {
 
 		apresentacao(Memoria, Processos);
 
-	}
-
-	public static void apresentacao(ArrayList<Memoria> Memoria, ArrayList<Processo> Processos) {
-
-		System.out.println("Estado |   Particao base | particao final");
-		System.out.println("--------------------------------------");
-		for (Memoria m : Memoria) {
-			System.out.println(
-					m.getEstado() + "      |        " + m.getParticaoBase() + "        | " + m.getParticaoFinal());
-			System.out.println("--------------------------------------");
-		}
-
-		System.out.println("Computacao |   Alocado | Visitado");
-		System.out.println("--------------------------------------");
-		for (Processo p : Processos) {
-			System.out.println(p.getComputacao() + "      |        " + p.getAlocado() + "        | " + p.getVisitado());
-			System.out.println("--------------------------------------");
-		}
+		firstFit(Memoria, Processos);
 
 	}
 
-	public static void firstFit(ArrayList<Memoria> strMemoria, ArrayList<Processo> strProcessos) {
+	public static void firstFit(ArrayList<Memoria> Memoria, ArrayList<Processo> Processos) {
 		Boolean tudoAlocado = true;
 
-		for (Processo processo : strProcessos) {
-			for (Memoria memoria : strMemoria) {
-				if (memoria.getEstado().equals("H")) {
+		for (Processo processo : Processos) {
+			processo.setVisitado(1);
+			for (Memoria memoria : Memoria) {
+				if (memoria.getEstado().equals("H")
+						&& ((memoria.getParticaoBase() - memoria.getParticaoFinal()) <= processo.getComputacao())) {
 					memoria.setEstado("P");
 					memoria.setIdProcesso(processo.getId());
+					processo.setAlocado(1);
 
 				}
 			}
+		}
+
+		apresentacaoResultado(Memoria, Processos);
+
+	}
+
+	public static void apresentacaoResultado(ArrayList<Memoria> Memoria, ArrayList<Processo> Processos) {
+		System.out.println("");
+		System.out.println("");
+		System.out.print("Estado |   Particao base | particao final");
+		System.out.print(" ----- |||| ----");
+		System.out.print("Computacao |   Alocado ");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		Boolean acabouMemoria = false;
+		Boolean acabouProcesso = false;
+		for (int i = 0;; i++) {
+			if (i < Memoria.size()) {
+				System.out.print(Memoria.get(i).getEstado() + "      |        " + Memoria.get(i).getParticaoBase()
+						+ "        | " + Memoria.get(i).getParticaoFinal() + "                  ");
+
+			} else {
+				acabouMemoria = true;
+			}
+			
+		
+			
+			if (i < Processos.size()) {
+				if (Processos.get(i).getAlocado() == 0) {
+					System.out.print("          " + Processos.get(i).getComputacao() + "  |  " + "não alocado" + "| ");
+				} else {
+					System.out.print("          " + Processos.get(i).getComputacao() + " |  " + "alocado" + "| ");
+				}
+				
+			} else {
+				acabouProcesso = true;
+			}
+			if (acabouMemoria && acabouProcesso) {
+
+				break;
+			}
+			System.out.println("");
+			System.out.print("--------------------------------------");
+			System.out.print("        ");
+			System.out.print("          ------------------------");
+			System.out.println("");
 		}
 
 	}
@@ -99,6 +132,25 @@ public class gerenciamentoMemoriaMain {
 
 			return null;
 
+		}
+
+	}
+
+	public static void apresentacao(ArrayList<Memoria> Memoria, ArrayList<Processo> Processos) {
+
+		System.out.println("Estado |   Particao base | particao final");
+		System.out.println("--------------------------------------");
+		for (Memoria m : Memoria) {
+			System.out.println(
+					m.getEstado() + "      |        " + m.getParticaoBase() + "        | " + m.getParticaoFinal());
+			System.out.println("--------------------------------------");
+		}
+
+		System.out.println("Computacao |   Alocado | Visitado");
+		System.out.println("--------------------------------------");
+		for (Processo p : Processos) {
+			System.out.println(p.getComputacao() + "      |        " + p.getAlocado() + "        | " + p.getVisitado());
+			System.out.println("--------------------------------------");
 		}
 
 	}
